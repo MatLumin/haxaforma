b3a778d1b86bfb454136c7a10dae301bf5275fbe
user_07 <1.brightNed@gmail.com>
2024-10-28 19:03:09 +0330

    made ui better

--- board_codes/main.py/main.py.ino
+++ board_codes/main.py/main.py.ino
@@ -5,166 +5,250 @@
 #include <WiFi.h>
 #include "SPIFFS.h"
 
+
 const String INDEX_HTML = R"V0G0N(
 <style>
+
+:root 
+  {
+  --nik_comp_color:#e02e28;
+  --nik_comp_bg_color_red:#180000;
+
+  }
+
+
+body
+  {
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(0deg, #1a1a1a, rgb(34, 34, 34));
+  }
+
+html
+  {
+  width: 100vw;
+  height: 100vh;
+  }
+
+
 *
-	{
-	background: black;
-	color: wheat;
-	font-family: monospace;
-	border-radius: 3px;
-	}
+  {
+  font-family: sans-serif;
+  font-size: 3vh;
+  border-radius: 2vh;
+  font-weight: bold;
+  }
+
 input
-	{
-	display: block;
-	background: white;
-	color: black;
-	width: 40vh;
-	margin-bottom: 2vh;
-	padding: 3vh;
-	font-size: 12;
-	}
+  {
+  display: block;
+  background: white;
+  color: black;
+  width: 80%;
+  margin-bottom: 5%;
+  padding-top: 2%;
+  padding-left: 0;
+  padding-right: 0;
+  padding-bottom: 2%;
+  text-align:center;
+  border-color:black;
+  border-width: 2px;
+  border-style: solid;
+  -webkit-appearance: none;
+  -moz-appearance: textfield;
+
+  }
+
+input:focus
+{
+  border-color: var(--nik_comp_color);
+  outline-style: none;
+
+
+
+}
+
+
+
+
 button
-	{
+  {
+  border-style: none;
+  padding: 5%;
+  transition: 0.3s;
+  background-color: var(--nik_comp_color);
+  color: white;
+  }
 
-	}
+button:hover
+  {
+  transform: translateY(-20px);
+  transition: 0.3s;
+  }
+
+button:active
+  {
+  transform: translateY(+20px); 
+  transition: 0.3s;
+  }
+
+
+
+#main_form
+  {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  }
+
+
+label
+  {
+  background: -webkit-linear-gradient(var(--nik_comp_color), var(--nik_comp_color));
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+
 </style>
+
 <title>HEXA FORM</title>
-<form > 
-	<label>CELL 0</label>
-	<input type="number" value="HEX_VALUE_PLACE_HOLDER_0" name="0" min="0" max="255">
 
-	<label>CELL 1</label>
-	<input type="number" value="HEX_VALUE_PLACE_HOLDER_1" name="1" min="0" max="255">
 
-	<label>CELL 2</label>
-	<input type="number" value="HEX_VALUE_PLACE_HOLDER_2" name="2" min="0" max="255">
+<form id="main_form"> 
+  <label>Field 0</label>
+  <input type="number" placeholder="0" name="0" min="0" max="255">
 
-	<label>CELL 3</label>
-	<input type="number" value="HEX_VALUE_PLACE_HOLDER_3" name="3" min="0" max="255">
+  <label>Field 1</label>
+  <input type="number" placeholder="0" name="1" min="0" max="255">
 
-	<label>CELL 4</label>
-	<input type="number" value="HEX_VALUE_PLACE_HOLDER_4" name="4" min="0" max="255">
+  <label>Field 2</label>
+  <input type="number" placeholder="0" name="2" min="0" max="255">
 
-	<label>CELL 5</label>
-	<input type="number" value="HEX_VALUE_PLACE_HOLDER_5" name="5" min="0" max="255">
+  <label>Field 3</label>
+  <input type="number" placeholder="0" name="3" min="0" max="255">
 
-	<button type="submit"> SUBMIT!</button>	 
+  <label>Field 4</label>
+  <input type="number" placeholder="0" name="4" min="0" max="255">
+
+  <label>Field 5</label>
+  <input type="number" placeholder="0" name="5" min="0" max="255">
+
+  <button type="submit"> Submit!</button>  
 </form>
 )V0G0N";
 
-const char* ssid = "HEX_FORMA";
-const char* password = "1243456789";
+
+
+
+
 
 
 WebServer server(80);
 
 
+
+
+
 void hh_ui_index()
-  {
-  int hexa0 = server.arg("0").toInt();
-  int hexa1 = server.arg("1").toInt();
-  int hexa2 = server.arg("2").toInt();
-  int hexa3 = server.arg("3").toInt();
-  int hexa4 = server.arg("4").toInt();
-  int hexa5 = server.arg("5").toInt();
+{
+  // استفاده از strtol برای تبدیل رشته هگزادسیمال به عدد
+  int hexa0 = strtol(server.arg("0").c_str(), NULL, 16);
+  int hexa1 = strtol(server.arg("1").c_str(), NULL, 16);
+  int hexa2 = strtol(server.arg("2").c_str(), NULL, 16);
+  int hexa3 = strtol(server.arg("3").c_str(), NULL, 16);
+  int hexa4 = strtol(server.arg("4").c_str(), NULL, 16);
+  int hexa5 = strtol(server.arg("5").c_str(), NULL, 16);
 
+  // ذخیره‌سازی اعداد در فایل
   File file = SPIFFS.open("/data.txt", FILE_WRITE);
-
   file.println(hexa0);
   file.println(hexa1);
   file.println(hexa2);
   file.println(hexa3);
   file.println(hexa4);
   file.println(hexa5);
-
   file.close();
 
+  // خواندن داده‌ها از فایل برای نمایش مجدد در فرم
   file = SPIFFS.open("/data.txt", FILE_READ);
 
   int array_index = 0;
-  int data[6] = {0,0,0,0,0,0};
+  int data[6] = {0, 0, 0, 0, 0, 0};
   String buffer = "";
 
-
-  while(file.available())
-    {
+  while (file.available())
+  {
     char charcode = (file.read());
     if (charcode == 10)
-      {
+    {
       data[array_index] = buffer.toInt();
       buffer = "";
       array_index += 1;
-      }
-    buffer += String(charcode);
     }
-  
-  file.close();
-
-  String output = INDEX_HTML;
-  int x = 10;
-  
-
-  output.replace("HEX_VALUE_PLACE_HOLDER_0", String(data[0]));
-  output.replace("HEX_VALUE_PLACE_HOLDER_1", String(data[1]));
-  output.replace("HEX_VALUE_PLACE_HOLDER_2", String(data[2]));
-  output.replace("HEX_VALUE_PLACE_HOLDER_3", String(data[3]));
-  output.replace("HEX_VALUE_PLACE_HOLDER_4", String(data[4]));
-  output.replace("HEX_VALUE_PLACE_HOLDER_5", String(data[5]));
-  
-
-  server.send(200, "text/html", output); 
+    buffer += String(charcode);
   }
 
-void setup() {
+  file.close();
+
+  // آماده‌سازی صفحه HTML با جایگزینی مقادیر هگزادسیمال
+  String output = INDEX_HTML;
+  output.replace("HEX_VALUE_PLACE_HOLDER_0", "0x" + String(data[0], HEX));
+  output.replace("HEX_VALUE_PLACE_HOLDER_1", "0x" + String(data[1], HEX));
+  output.replace("HEX_VALUE_PLACE_HOLDER_2", "0x" + String(data[2], HEX));
+  output.replace("HEX_VALUE_PLACE_HOLDER_3", "0x" + String(data[3], HEX));
+  output.replace("HEX_VALUE_PLACE_HOLDER_4", "0x" + String(data[4], HEX));
+  output.replace("HEX_VALUE_PLACE_HOLDER_5", "0x" + String(data[5], HEX));
+  output += WiFi.softAPmacAddress();
+  // ارسال صفحه HTML به کلاینت
+  server.send(200, "text/html", output);
+}
+
+void setup()
+{
   Serial.begin(9600);
 
-	if (!SPIFFS.begin(true)) 
-    {
-	  Serial.println("An error has occurred while mounting SPIFFS");
-	  return;
-	  }
-  File file = SPIFFS.open("/data.txt");
-  if (file.available())
-    {
-    Serial.write("file data.txt exists; so NO new file will be created");
-    }
-  else
-    {
-    Serial.write("file data.txt does not exist; so new file will be created");  
-    file.close();
-    file = SPIFFS.open("/data.txt", FILE_WRITE);
-    file.println(0); 
-    file.println(0);
-    file.println(0);
-    file.println(0);
-    file.println(0);
-    file.println(0);
-    }
   
 
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("An error has occurred while mounting SPIFFS");
+    return;
+  }
 
+  // بررسی وجود فایل data.txt و ایجاد آن در صورت عدم وجود
+  File file = SPIFFS.open("/data.txt");
+  if (!file)
+  {
+    Serial.write("file data.txt does not exist; creating new file");
+    file = SPIFFS.open("/data.txt", FILE_WRITE);
+    for (int i = 0; i < 6; i++)
+    {
+      file.println(0); // مقدار اولیه صفر برای هر سلول
+    }
+    file.close();
+  }
+  else
+  {
+    Serial.write("file data.txt exists");
+    file.close();
+  }
 
-  file.close();
-  file = SPIFFS.open("/test.txt");
-
-  Serial.write(file.read());
-  file.close();
-
-  WiFi.softAP(ssid, password);
+  delay(1200);
+  const String ssid = String("NIK_GTW_")+String(WiFi.softAPmacAddress()).substring(12);
+  WiFi.softAP(ssid, "12345678");
 
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
 
+  // تنظیم روت برای نمایش فرم
   server.on("/index", hh_ui_index);
 
   server.begin();
-
 }
 
-
-void loop() 
-  {
+void loop()
+{
   server.handleClient();
-  }
+}
